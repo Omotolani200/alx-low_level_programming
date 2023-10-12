f3ee9b9f75f751bfd5908a97ef50880d85e14bec
Omotolani200 <okulajasherifffdeen@gmail.com>
2023-10-12 14:28:32 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-main.c
+++ 0x10-variadic_functions/1-main.c
@@ -1,7 +1,4 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
+#include "variadic_functions.h"
 
 /**
  * main - check the code
@@ -10,26 +7,7 @@
  */
 int main(void)
 {
-    list_t *head;
-    list_t *new;
-    list_t hello = {"World", 5, NULL};
-    size_t n;
-
-    head = &hello;
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->str = strdup("Hello");
-    new->len = 5;
-    new->next = head;
-    head = new;
-    n = list_len(head);
-    printf("-> %lu elements\n", n);
-    free(new->str);
-    free(new);
+    print_numbers(", ", 4, 0, 98, -1024, 402);
     return (0);
 }
 
