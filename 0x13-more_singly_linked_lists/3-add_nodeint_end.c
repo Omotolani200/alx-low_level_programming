c4f3253dcc079b7c766f77dbce386607e1c22a88
Omotolani200 <okulajasherifffdeen@gmail.com>
2023-10-23 22:56:08 -0700

    update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,40 +1,35 @@
 #include "lists.h"
-
+#include <stdlib.h>
 /**
- * add_nodeint_end - Adds a new node at the end of a listint_t list.
- * @head: A pointer to a pointer to the head of the list.
- * @n: The integer to be stored in the new node.
- *
- * Return: The address of the new element, or NULL if it fails.
- */
+  * add_nodeint_end - add node at end of a listint_t list.
+  *
+  * @head: head of double pointer
+  * @n: int add the list
+  * Return: NULL if it failed
+  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *newNode;
-	listint_t *temp = *head; /* Create a temporary pointer to traverse the list */
+	listint_t *new;
+	listint_t *temp;
 
-	newNode = malloc(sizeof(listint_t)); /* Allocate memory for the new node */
-	if (newNode == NULL)
-	{
-		return (NULL); /* Return NULL if memory allocation fails */
-	}
-
-	newNode->n = n; /* Set the data of the new node */
-	newNode->next = NULL; /*Initialize the next pointer of the new node to NULL */
-
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
 	if (*head == NULL)
 	{
-		*head = newNode; /* If the list is empty, make the new node the head */
+	*head = new;
+		return (new);
 	}
-	else
+	temp = *head;
+	while (temp->next != NULL)
 	{
-		while (temp->next != NULL)
-		{
-			temp = temp->next; /* Traverse to the last node */
-		}
-		temp->next = newNode;
-		/* Update the last node's next pointer to point to the new node */
+		temp = temp->next;
 	}
-
-	return (newNode); /* Return the address of the new element */
+	temp->next = new;
+	return (new);
 }
 
